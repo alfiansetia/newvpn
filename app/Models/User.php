ed7c631c06c0f54cdad8e946fe0cb77974aa06da
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-11-17 22:44:12 +0700

    MOVE EMAIL AUTH TO QUEUE

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPasswordNotification;
+use App\Notifications\VerifyEmailNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -66,6 +68,16 @@ class User extends Authenticatable implements MustVerifyEmail
             'is_active'         => 'boolean',
             'router_limit'      => 'integer',
         ];
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmailNotification);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
     }
 
     public function scopeFilter($query, array $filters)
